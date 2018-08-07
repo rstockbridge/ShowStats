87d1e609f9a5145b4a0f6a459b7f2f146eb634e7
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2018-08-07 14:39:04 -0400

    Initialize app

---
+++ app/src/main/java/com/github/rstockbridge/showstats/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.rstockbridge.showstats;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
