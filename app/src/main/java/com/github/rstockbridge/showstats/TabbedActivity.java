5ec0b8b93ab9bf6a9f2fa3d2c2ae2879749f3277
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2018-08-31 13:43:48 -0400

    Increase ViewPager offscreen page limit to cover all tabs
    Tab fragments will no longer be destroyed and recreated.

--- app/src/main/java/com/github/rstockbridge/showstats/TabbedActivity.java
+++ app/src/main/java/com/github/rstockbridge/showstats/TabbedActivity.java
@@ -15,6 +15,7 @@ public final class TabbedActivity extends AppCompatActivity {
 
         final ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        viewPager.setOffscreenPageLimit(3);
 
         final TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
