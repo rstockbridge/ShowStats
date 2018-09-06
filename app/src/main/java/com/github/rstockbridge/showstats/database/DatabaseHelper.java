86d902dbafda40153864866f972ae5c42ed6d3e6
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2018-09-06 18:10:04 -0400

    Add ability to save show notes
    Click on a show in the list of shows tab to edit and save notes.

--- app/src/main/java/com/github/rstockbridge/showstats/database/DatabaseHelper.java
+++ app/src/main/java/com/github/rstockbridge/showstats/database/DatabaseHelper.java
@@ -14,12 +14,18 @@ import java.util.Map;
 public final class DatabaseHelper {
 
     private static final String USERS_PATH = "users";
-    private static final String USER_ID_KEY = "userId";
+    private static final String SETLISTFM_USER_ID_KEY = "userId";
+    private static final String SHOWS_PATH = "shows";
+    private static final String SHOW_NOTES_KEY = "showNotesKey";
 
     public interface SetlistfmUserListener {
         void onStoredSetlistfmUser(final String setlistfmUserId);
 
         void onNoStoredSetlistfmUser();
+    }
+
+    public interface ShowNotesListener {
+        void onShowNotes(final String notes);
     }
 
     public interface UpdateDatabaseListener {
@@ -49,9 +55,9 @@ public final class DatabaseHelper {
             public void onSuccess(final DocumentSnapshot document) {
                 if (document.exists()
                         && document.getData() != null
-                        && document.getData().get(USER_ID_KEY) != null) {
+                        && document.getData().get(SETLISTFM_USER_ID_KEY) != null) {
 
-                    listener.onStoredSetlistfmUser((String) document.getData().get(USER_ID_KEY));
+                    listener.onStoredSetlistfmUser((String) document.getData().get(SETLISTFM_USER_ID_KEY));
                 } else {
                     listener.onNoStoredSetlistfmUser();
                 }
@@ -64,16 +70,63 @@ public final class DatabaseHelper {
         });
     }
 
-    public void updateDatabase(
+    public void updateSetlistfmUserInDatabase(
             @NonNull final String authUserUid,
             @NonNull final String setlistfmUserId,
             @NonNull final UpdateDatabaseListener listener) {
 
-        final Map<String, Object> textData = new HashMap<>();
-        textData.put(USER_ID_KEY, setlistfmUserId);
+        final Map<String, Object> setlistfmUserIdData = new HashMap<>();
+        setlistfmUserIdData.put(SETLISTFM_USER_ID_KEY, setlistfmUserId);
 
-        database.collection(USERS_PATH).document(authUserUid)
-                .set(textData)
+        database.collection(USERS_PATH)
+                .document(authUserUid)
+                .set(setlistfmUserIdData)
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        listener.onUpdateDatabaseUnsuccessful();
+                    }
+                });
+    }
+
+    public void getShowNotes(
+            @NonNull final String authUserUid,
+            @NonNull final String showId,
+            @NonNull final ShowNotesListener listener) {
+
+        final DocumentReference docRef = database
+                .collection(USERS_PATH)
+                .document(authUserUid)
+                .collection(SHOWS_PATH)
+                .document(showId);
+
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(final DocumentSnapshot document) {
+                if (document.exists()
+                        && document.getData() != null
+                        && document.getData().get(SHOW_NOTES_KEY) != null) {
+
+                    listener.onShowNotes((String) document.getData().get(SHOW_NOTES_KEY));
+                }
+            }
+        });
+    }
+
+    public void updateShowNotesInDatabase(
+            @NonNull final String authUserUid,
+            @NonNull final String showId,
+            @NonNull final String showNotes,
+            @NonNull final UpdateDatabaseListener listener) {
+
+        final Map<String, Object> showNotesData = new HashMap<>();
+        showNotesData.put(SHOW_NOTES_KEY, showNotes);
+
+        database.collection(USERS_PATH)
+                .document(authUserUid)
+                .collection(SHOWS_PATH)
+                .document(showId)
+                .set(showNotesData)
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
