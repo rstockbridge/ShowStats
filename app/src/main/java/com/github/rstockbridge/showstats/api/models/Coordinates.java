42907ab75c3d3018e7f59d052c23b189b39f3375
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2018-09-07 15:19:05 -0400

    Change map markers to cities rather than venues
    Venues only have lat/long data for the city they're in rather
    than their specific location, so change the display to reflect
    cities only.

--- app/src/main/java/com/github/rstockbridge/showstats/api/models/Coordinates.java
+++ app/src/main/java/com/github/rstockbridge/showstats/api/models/Coordinates.java
@@ -2,6 +2,8 @@ package com.github.rstockbridge.showstats.api.models;
 
 import com.squareup.moshi.Json;
 
+import java.util.Objects;
+
 public final class Coordinates {
 
     @Json(name = "lat")
@@ -16,5 +18,20 @@ public final class Coordinates {
 
     public double getLongitude() {
         return longitude;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Coordinates that = (Coordinates) o;
+        return Double.compare(that.latitude, latitude) == 0 &&
+                Double.compare(that.longitude, longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(latitude, longitude);
     }
 }
